29ba57656963da5188032a58e4423df2d1e9ffb4
zilani131 <zilani.cuet@gmail.com>
2022-03-11 09:01:25 +0600

    display content done

--- js/app.js
+++ js/app.js
@@ -27,7 +27,8 @@ const reportPost = (id) => {
 };
 
 const displayContent = (text) => {
-    return text.length < 30 ? 'text' : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
+    
+    return text.length < 30 ? text.slice(0, 30) : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
 };
 
 const switchTab = (id) => {
